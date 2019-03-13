998b92aa5b794eed9e9b4d7e07267438f475932c
Cory <cory.forsyth@gmail.com>
2019-03-13 18:40:54 -0400

    finish implementing reading of all gzip headers

--- src/gzip.rs
+++ src/gzip.rs
@@ -1,16 +1,21 @@
+#[derive(Debug)]
 pub struct Gzip {
-  headers: Headers,
+  pub headers: Headers,
   crc32: u32,
   size: u32,
 }
 
-// impl Gzip {
-//   fn new(bytes: Vec<u8>) -> Gzip {
-//     let mut bytes = bytes.iter();
-//     let headers = Headers::new(&mut bytes);
-//     Gzip { headers }
-//   }
-// }
+impl Gzip {
+  pub fn new(bytes: Vec<u8>) -> Gzip {
+    let mut bytes = bytes.iter();
+    let headers = Headers::new(&mut bytes);
+    Gzip {
+      headers,
+      crc32: 0,
+      size: 0,
+    }
+  }
+}
 
 // Compression
 // Flags
@@ -22,52 +27,22 @@ pub struct Gzip {
 // Optional filename
 // Optional comment
 // Optional crc16
+#[derive(Debug)]
 pub struct Headers {
-  compression: Compression,
-  mtime: u32,
-  os: Os,
-  filename: Option<String>,                 // indicated by flags bit 3
-  comment: Option<String>,                  // indicated by flags bit 4
-  crc16: Option<String>,                    // indicated by flags bit 1
-  compressionInfo: Option<CompressionInfo>, // via "extra flags"
-  isText: bool,                             // == Flags bit 0
-  extraFields: Vec<ExtraField>,
+  pub compression: Compression,
+  pub mtime: u32,
+  pub os: Os,
+  pub filename: Option<String>, // indicated by flags bit 3
+  pub comment: Option<String>,  // indicated by flags bit 4
+  pub crc16: Option<u32>,       // indicated by flags bit 1
+  pub compression_info: Option<CompressionInfo>, // via "extra flags"
+  pub is_text: bool,            // == Flags bit 0
+  pub extra_fields: Vec<ExtraField>,
 }
 
-const MAGIC_BYTES: (u8, u8) = (0x1f, 0x8b);
-
-type ByteIterator = Iterator<Item = u8>;
-
-// impl Headers {
-//   fn new(bytes: &mut ByteIterator) -> Headers {
-//     match (bytes.next(), bytes.next()) {
-//       (Some(0x1f), Some(0x8b)) => (),
-//       _ => panic!("Got wrong initial bytes"),
-//     }
-
-//     let byte = bytes.next().unwrap();
-//     let compression = Compression::parse(byte);
-
-//     let flags = bytes.next().unwrap();
-//     let mtime = read_int(bytes, 4);
-//   }
-// }
-
-fn read_int(bytes: &mut ByteIterator, size: usize) -> u32 {
-  let mut values = vec![];
-  while values.len() < size {
-    let byte = bytes.next().unwrap();
-    values.push(byte);
-  }
-  values
-    .iter()
-    .map(|&v| u32::from(v))
-    .enumerate()
-    .fold(0, |acc, (idx, val)| {
-      dbg!((acc, idx, val));
-      acc + dbg!(val << (8 * idx))
-    })
-}
+// TODO -- I cannot figure out how to use
+// this in the match below. `MAGIC_BYTES[0]` does not seem to be syntactically valid
+const MAGIC_BYTES: [u8; 2] = [0x1f, 0x8b];
 
 enum Flags {
   Text = 0b1,
@@ -77,11 +52,122 @@ enum Flags {
   Comment = 0b10000,
 }
 
-struct ExtraField {
-  id: String,
-  value: String,
+impl Headers {
+  fn new<'a, I: Iterator<Item = &'a u8>>(bytes: &mut I) -> Headers {
+    match (bytes.next(), bytes.next()) {
+      (Some(0x1f), Some(0x8b)) => (),
+      _ => panic!("Got wrong initial bytes"),
+    }
+
+    let byte = bytes.next().unwrap();
+    let compression = Compression::parse(*byte);
+
+    let flags = bytes.next().unwrap();
+    let mtime = read_int(bytes, 4);
+
+    // todo use this
+    let extra_flag = bytes.next().unwrap();
+    let compression_info = CompressionInfo::parse(*extra_flag);
+
+    let os = Os::parse(*bytes.next().unwrap());
+
+    let extra_fields = if *flags & Flags::Extra as u8 != 0 {
+      // parse extra fields
+      let mut len = read_int(bytes, 2);
+      let mut result = vec![];
+
+      while len > 0 {
+        let (bytes_read, field) = read_extra_data_field(bytes);
+        len -= bytes_read;
+        result.push(field);
+      }
+      result
+    } else {
+      vec![]
+    };
+
+    let filename = if *flags & Flags::FileName as u8 != 0 {
+      Some(read_ascii_string(bytes))
+    } else {
+      None
+    };
+
+    let comment = if *flags & Flags::Comment as u8 != 0 {
+      Some(read_ascii_string(bytes))
+    } else {
+      None
+    };
+
+    let is_text = *flags & Flags::Text as u8 != 0;
+
+    let crc16 = if *flags & Flags::CRC16 as u8 != 0 {
+      Some(read_int(bytes, 2))
+    } else {
+      None
+    };
+
+    Headers {
+      compression,
+      mtime,
+      os,
+      filename,
+      comment,
+      crc16,
+      compression_info,
+      is_text,
+      extra_fields,
+    }
+  }
 }
 
+fn read_extra_data_field<'a, I: Iterator<Item = &'a u8>>(bytes: &mut I) -> (u32, ExtraField) {
+  let mut id = String::new();
+  id.push(*bytes.next().unwrap() as char);
+  id.push(*bytes.next().unwrap() as char);
+
+  let len = read_int(bytes, 2);
+  let mut data = String::new();
+  for _ in 0..len {
+    data.push(*bytes.next().unwrap() as char);
+  }
+
+  (len + 4, ExtraField { id, data })
+}
+
+// Read little-endian int of `size` bytes
+fn read_int<'a, I: Iterator<Item = &'a u8>>(bytes: &mut I, size: usize) -> u32 {
+  let mut values = vec![];
+  while values.len() < size {
+    let byte = bytes.next().unwrap();
+    values.push(byte);
+  }
+  values
+    .iter()
+    .map(|&v| u32::from(*v))
+    .enumerate()
+    .fold(0, |acc, (idx, val)| acc + (val << (8 * idx)))
+}
+
+// Read null-terminated string
+fn read_ascii_string<'a, I: Iterator<Item = &'a u8>>(bytes: &mut I) -> String {
+  let mut result = String::new();
+  loop {
+    match bytes.next() {
+      Some(b'\0') => break,
+      Some(&v) => result.push(v as char),
+      None => break,
+    }
+  }
+  result
+}
+
+#[derive(Debug)]
+pub struct ExtraField {
+  pub id: String,
+  pub data: String,
+}
+
+#[derive(Debug)]
 pub enum Compression {
   Deflate,
 }
@@ -95,11 +181,23 @@ impl Compression {
   }
 }
 
+#[derive(Debug)]
 pub enum CompressionInfo {
   MaximumCompressionSlowestAlgorithm,
   FastestAlgorithm,
 }
 
+impl CompressionInfo {
+  fn parse(byte: u8) -> Option<CompressionInfo> {
+    match byte {
+      2 => Some(CompressionInfo::MaximumCompressionSlowestAlgorithm),
+      4 => Some(CompressionInfo::FastestAlgorithm),
+      _ => None,
+    }
+  }
+}
+
+#[derive(Debug)]
 pub enum Os {
   FATFilesystem,
   Amiga,
@@ -118,6 +216,30 @@ pub enum Os {
   Unknown,
 }
 
+impl Os {
+  fn parse(byte: u8) -> Os {
+    use Os::*;
+    match byte {
+      0 => FATFilesystem,
+      1 => Amiga,
+      2 => VMS,
+      3 => Unix,
+      4 => VMcMS,
+      5 => AtaritOS,
+      6 => HPFS,
+      7 => Macintosh,
+      8 => Zsystem,
+      9 => CPm,
+      10 => TOPS20,
+      11 => NTFS,
+      12 => QDOS,
+      13 => Acorn,
+      255 => Unknown,
+      _ => panic!("Unexpected OS value {}", byte),
+    }
+  }
+}
+
 #[cfg(test)]
 mod test {
   use super::*;
@@ -128,24 +250,24 @@ mod test {
     // let mut bytes = bytes.iter().cloned();
     // assert_eq!(read_int(&mut bytes, 4), 0);
 
-    let bytes = &[0b1, 0b0, 0b0, 0b0];
-    let mut bytes = bytes.iter().cloned();
+    let bytes = [0b1, 0b0, 0b0, 0b0];
+    let mut bytes = bytes.iter();
     assert_eq!(read_int(&mut bytes, 4), 1);
 
     let bytes = &[0b0, 0b1, 0b0, 0b0];
-    let mut bytes = bytes.iter().cloned();
+    let mut bytes = bytes.iter();
     assert_eq!(read_int(&mut bytes, 4), 256);
 
     let bytes = &[0b0, 0b0, 0b1, 0b0];
-    let mut bytes = bytes.iter().cloned();
+    let mut bytes = bytes.iter();
     assert_eq!(read_int(&mut bytes, 4), 0x0001_0000);
 
     let bytes = &[0b0, 0b0, 0b0, 0b1];
-    let mut bytes = bytes.iter().cloned();
+    let mut bytes = bytes.iter();
     assert_eq!(read_int(&mut bytes, 4), 0x0100_0000);
 
     let bytes = &[0b0000_0000, 0b1111_1111, 0b0000_0000, 0b0000_1000];
-    let mut bytes = bytes.iter().cloned();
+    let mut bytes = bytes.iter();
     assert_eq!(read_int(&mut bytes, 4), 0x0800_ff00);
   }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ use std::fs::File;
 use std::io::Read;
 
 pub mod gzip;
+use gzip::Gzip;
 
 pub struct Config {
   pub filename: String,
@@ -23,9 +24,45 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
   let mut buf = vec![];
   let mut file = File::open(&config.filename)?;
   let num_read = file.read_to_end(&mut buf)?;
-  println!("Read {} from {}:", num_read, &config.filename);
-  for b in &buf {
-    println!("{}", b);
-  }
+  println!("Read {} bytes from {}", num_read, &config.filename);
+  let gzip = Gzip::new(buf);
+  print_gzip_info(gzip);
   Ok(())
+}
+
+use chrono::NaiveDateTime;
+fn print_gzip_info(gz: Gzip) {
+  println!("Gzip Info");
+  println!("Compression: {:?}", gz.headers.compression);
+  println!(
+    "Modification Time: {} ({})",
+    NaiveDateTime::from_timestamp(i64::from(gz.headers.mtime), 0),
+    gz.headers.mtime,
+  );
+  println!("Os: {:?}", gz.headers.os);
+  match gz.headers.filename {
+    Some(string) => println!("Original Filename: {}", string),
+    None => println!("Original Filename: <unknown>"),
+  }
+
+  match gz.headers.comment {
+    Some(string) => println!("Comment: {}", string),
+    None => println!("Comment: <none>"),
+  }
+
+  match gz.headers.crc16 {
+    Some(v) => println!("Headers CRC16: {}", v),
+    None => println!("Headers CRC16: <none>"),
+  }
+
+  match gz.headers.compression_info {
+    Some(info) => println!("Compression info: {:?}", info),
+    None => println!("Compression info: <none>"),
+  }
+
+  println!("Is Text Flag: {}", gz.headers.is_text);
+  println!("{} Extra Fields", gz.headers.extra_fields.len());
+  for extra_field in gz.headers.extra_fields {
+    println!("\t {}: {}", extra_field.id, extra_field.data);
+  }
 }
